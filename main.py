233d6870ec7acd2b414c546658d0530cbfb74762
Gianni Costanzi <gianni.costanzi@gmail.com>
2022-06-21 15:19:04 +0200

    Moved sleep at the beginning to avoid repeated frequent errors

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ with open("config.json") as json_conf:
     conf = json.load(json_conf)
 
     while(True):
+        # Sleep at the beginning of the loop since if the program crashes due to a network error, for example, the container is restarted and it would
+        # immediately experience the same crash. In this way if the program is restarted we wait sleepMinutes minutes before retrying again.
+        time.sleep(conf["sleepMinutes"] * 60)
         counter += 1
         with open("doctors.json") as json_doctors:
             doctors = json.load(json_doctors)
@@ -60,4 +63,3 @@ with open("config.json") as json_conf:
 
                 print("Sending test message")
                 sendEmailViaGmailSMPT(conf["senderEmailAddress"], conf["senderPassword"], conf["adminEmail"], msg.as_string())      
-        time.sleep(conf["sleepMinutes"] * 60)
